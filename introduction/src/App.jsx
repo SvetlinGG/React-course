777382752058dbb6cb36a438a3caa01e7cf714ff
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-13 21:58:09 +0200

    two way of function declaration

--- introduction/src/App.jsx
+++ introduction/src/App.jsx
@@ -1,14 +1,20 @@
 
-import './App.css'
 
-function App() {
+// function declaration component examle
+//function App() {
   
+  // return (
+  //   <>
+  //     <h1>Hello, React</h1>
+  //   </>
+    
+  // )
+  // Declaration with arrow function
+  const App = () => {
+    const element = <h1>Hello, from React</h1>
 
-  return (
-    <>
-    <h1>Hello React</h1>
-    </>
-  )
-}
+    return element
+  }
+//}
 
 export default App
