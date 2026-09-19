a15d5bb6e0b8d0959ac0c07d156d2689cd2b1b4e
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-19 22:30:48 +0200

    next day click handler

--- component-deep-dive/src/components/Calendar.jsx
+++ component-deep-dive/src/components/Calendar.jsx
@@ -13,11 +13,19 @@ const days = [
 export default function Calendar() {
 
     const [day, setDay] = useState(0);
+
+    const nextDayClickHandler = () => {
+        setDay((prevDay) => (prevDay + 1) % days.length)
+    }
+
+
     return (
         <section>
             <h2>Calendar</h2>
 
             <div>Current Day {days.at(day)}</div>
+
+            <button onClick={nextDayClickHandler}>Next Day</button>
         </section>
     );
 }
