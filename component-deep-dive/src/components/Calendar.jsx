53052e18cd5b2cf758b22d8134722420a0472af2
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-19 21:44:52 +0200

    dya state

--- component-deep-dive/src/components/Calendar.jsx
+++ component-deep-dive/src/components/Calendar.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
+const days = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday'
+]
+
 export default function Calendar() {
+
+    const [day, setDay] = useState(0);
     return (
-        
+        <section>
+            <h2>Calendar</h2>
+
+            <div>Current Day {days.at(day)}</div>
+        </section>
     );
 }
