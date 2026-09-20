ae6b185d5fcff42a758995c91268106bad29ad36
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-20 21:10:15 +0200

    initilize the press button

--- component-deep-dive/src/components/Typer.jsx
+++ component-deep-dive/src/components/Typer.jsx
@@ -1,6 +1,13 @@
 export default function Typer() {
+
+    const keyDownHandler = (event) => {
+        console.log(event.key);
+        
+    }
+
+
     return (
-        <section>
+        <section tabIndex={0} onKeyDown={keyDownHandler}>
             <h1>Typer</h1>
             <p>Pressed Key</p>
 
