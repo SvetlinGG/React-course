9841bb04147dbd5a94f844a867c2b99eea9e9ecd
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-20 21:02:57 +0200

    typer component

--- component-deep-dive/src/App.jsx
+++ component-deep-dive/src/App.jsx
@@ -2,6 +2,7 @@ import Calendar from "./components/Calendar"
 import Counter from "./components/Counter"
 import MovieList from "./components/MovieList"
 import Timer from "./components/Timer"
+import Typer from "./components/Typer"
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
 
   return (
 
-    <>
+    <div className="components">
     <h1>React Timer</h1>
 
     <Timer />
@@ -20,7 +21,9 @@ function App() {
 
     <MovieList />
 
-    </>
+    <Typer />
+
+    </div>
   )
 }
 
---
+++ component-deep-dive/src/components/Typer.jsx
@@ -0,0 +1,10 @@
+export default function Typer() {
+    return (
+        <section>
+            <h1>Typer</h1>
+            <p>Pressed Key</p>
+
+            <strong>A</strong>
+        </section>
+    );
+}
