c02b2c68ad5504c130b10b37e17e044b3bbfa145
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-21 11:22:50 +0200

    style modules

--- component-deep-dive/src/App.jsx
+++ component-deep-dive/src/App.jsx
@@ -3,6 +3,7 @@ import Counter from "./components/Counter"
 import MovieList from "./components/MovieList"
 import TyperSection from "./components/Typer-section"
 import Timer from "./components/Timer"
+import StyledComponent from "./components/StyledComponent"
 
 
 function App() {
@@ -11,17 +12,19 @@ function App() {
   return (
 
     <div className="components">
-    <h1>React Timer</h1>
+    {/* <h1>React Timer</h1>
 
-    <Timer />
+    <Timer /> */}
 
-    <Counter />
+    {/* <Counter />
 
-    <Calendar />
+    <Calendar /> */}
 
     <MovieList />
 
     <TyperSection />
+
+    <StyledComponent />
 
 
     </div>
---
+++ component-deep-dive/src/components/StyledComponent.jsx
@@ -0,0 +1,12 @@
+export default function StyledComponent() {
+    return (
+        <section>
+            <h2>Styled Component</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur
+                 adipisicing elit. Ipsa rerum quasi,
+                  voluptatum fuga odit dolore. 
+                  Sequi et in odio reprehenderit.</p>
+        </section>
+    );
+}
