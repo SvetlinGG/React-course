5499ef161c8c9605a26378e104fb2b0cf93fe010
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-21 21:59:08 +0200

    footer component

--- component-deep-dive-EXE/src/App.jsx
+++ component-deep-dive-EXE/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 
@@ -524,7 +525,7 @@ function App() {
     </div> */}
   </main>
   {/* Footer component  */}
-  
+  <Footer />
 </>
 
     
